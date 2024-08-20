1e375ffa80efb706cd215b7f170df677e63fe820
richard <richard@iappsasia.com>
2024-08-20 21:45:06 +0800

    context & router

--- gee/gee.go
+++ gee/gee.go
@@ -1,43 +1,42 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandleFunc func(http.ResponseWriter, *http.Request)
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(*Context)
 
+// Engine implement the interface of ServeHTTP
 type Engine struct {
-	routerMap map[string]HandleFunc
+	router *router
 }
 
-func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := e.routerMap[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
-}
-
+// New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{routerMap: make(map[string]HandleFunc)}
+	return &Engine{router: newRouter()}
 }
 
-func (e *Engine) addRouter(method string, pattern string, f HandleFunc) {
-	key := method + "-" + pattern
-	e.routerMap[key] = f
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
 }
 
-func (e *Engine) Get(pattern string, f HandleFunc) {
-	e.addRouter("GET", pattern, f)
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
 }
 
-func (e *Engine) Post(pattern string, f HandleFunc) {
-	e.addRouter("POST", pattern, f)
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
 }
 
 // Run defines the method to start a http server
-func (e *Engine) Run(addr string) (err error) {
-	return http.ListenAndServe(addr, e)
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"gee/gee"
 	"net/http"
 )
 
 func main() {
 	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
 
-	r.Get("/hello", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "hello")
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run("localhost:8061")
-
 }
